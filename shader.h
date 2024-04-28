85c76076b89e3214da7269d8a2f3fbaff3ae9d11
nlGillity <dinozavar137@gmail.com>
2024-04-28 12:14:22 +0300

    Added uniforms in shader-class

--- shader.h
+++ shader.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <glm/glm.hpp>
+
 #include <glad/glad.h>
 #include <iostream>
 #include <sstream>
@@ -12,4 +14,50 @@ class Shader {
     public:
         Shader(const char*, const char*);
         void Use();
+
+        template <typename T>
+        void SetUniform(const char* name, T value) {
+            GLuint location = glGetUniformLocation(ID, name);
+            string tName = typeid(value).name();
+    
+            if (tName == typeid(GLuint).name())
+                glUniform1ui(location, value);
+            else if (tName == typeid(GLint).name())
+                glUniform1i(location, value);
+            else if (tName == typeid(GLfloat).name())
+                glUniform1f(location, value);
+            else
+                cout << "ERROR (UNIFORM): Failed to recognize uniform type" << endl;
+        }
+
+        template <typename T>
+        void SetUniformVec(const char* name, T &value) {
+            GLuint location = glGetUniformLocation(ID, name);
+            string tName = typeid(value).name();
+    
+            if (tName == typeid(glm::vec2).name())
+                glUniform2fv(location, 1, &value[0]);
+            else if (tName == typeid(glm::vec3).name())
+                glUniform3fv(location, 1, &value[0]);
+            else if (tName == typeid(glm::vec4).name())
+                glUniform4fv(location, 1, &value[0]);
+            else
+                cout << "ERROR (UNIFORM): Failed to recognize uniform vec type" << endl;
+        }
+
+        template <typename T>
+        void SetUniformMat(const char* name, T &value) {
+            GLuint location = glGetUniformLocation(ID, name);
+            string tName = typeid(value).name();
+    
+            if (tName == typeid(glm::mat2).name())
+                glUniformMatrix2fv(location, 1, GL_FALSE, &value[0][0]);
+            else if (tName == typeid(glm::mat3).name())
+                glUniformMatrix3fv(location, 1, GL_FALSE, &value[0][0]);
+            else if (tName == typeid(glm::mat4).name())
+                glUniformMatrix4fv(location, 1, GL_FALSE, &value[0][0]);
+            else
+                cout << "ERROR (UNIFORM): Failed to recognize uniform mat type" << endl;
+        }
+
 };
