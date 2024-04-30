e9cf06eb335a6e388349a13849d121737c60e263
nlGillity <dinozavar137@gmail.com>
2024-04-30 18:07:28 +0300

    Added parent primitive class

--- primitives.h
+++ primitives.h
@@ -1,8 +1,31 @@
 #pragma once
+#include <vector>
 #include <glad/glad.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+using namespace std;
 using namespace glm;
+
+class Primitive {
+private:
+    unsigned int vertex_num;
+    vec3* vertices;
+    GLuint myVAO, myVBO;
+
+    mat4 transform, commit_transform;
+public:
+    Primitive(int num, vec3* shape, vec3* color, vec3 position);
+    ~Primitive();
+
+    mat4* GetTransform();
+    void Translate(vec3 position);
+    void Rotate(float angle, vec3 axix);
+
+    void UpdateTransform();
+    void CommitTransform();
+
+    void Draw();
+};
 
 class Triangle {
 private:
@@ -13,6 +36,7 @@ private:
     };
     vec3 vertices[6];
     GLuint myVAO, myVBO;
+    mat4 commit_transform;
 
 public:
     vec3 color[3];
@@ -21,6 +45,11 @@ public:
     Triangle(vec3, vec3*);
     ~Triangle();
 
-    void Translate(vec3);
+    mat4* GetTransform();
+
+    void UpdateTransform();
+    void CommitTransform();
+    void Translate(vec3 position);
+    void Rotate(float angle, vec3 axix);
     void Draw();
 };
